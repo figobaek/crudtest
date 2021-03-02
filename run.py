9644e8ce903a0cca1e947d0bded05d2eed442ee1
figobaek <goldenboyy0524@gmail.com>
2021-03-02 20:16:42 +0900

    Heroku~~~~

--- run.py
+++ run.py
@@ -7,5 +7,6 @@ from app import create_app
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000")
+  app.run(host="0.0.0.0", port=port)
