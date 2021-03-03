8c89267d5beb7261f2ec719d1586da6664158167
figobaek <goldenboyy0524@gmail.com>
2021-03-04 01:44:08 +0900

    gg

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,3 @@
 
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://figobaek:dt2021@localhost:3306/dreamteam_db'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://figobaek:dt2021@127.0.0.1:3306/dreamteam_db'
--- run.py
+++ run.py
@@ -8,7 +8,7 @@ config_name = os.getenv('FLASK_CONFIG')
 
 app = create_app(config_name)
 app.config['SECRET_KEY'] = 'p9Bv<3Eid9%$i01'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://figobaek:dt2021@localhost:3306/dreamteam_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://figobaek:dt2021@127.0.0.1:3306/dreamteam_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
