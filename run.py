d697abb0d7200ad371562e4901c90aaf3b1b93d1
figobaek <goldenboyy0524@gmail.com>
2021-03-03 16:11:42 +0900

    gg

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ config_name = os.getenv('FLASK_CONFIG')
 
 app = create_app(config_name)
 app.config['SECRET_KEY'] = 'p9Bv<3Eid9%$i01'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@127.0.0.1/dreamteam_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@127.0.0.1:3306/dreamteam_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
