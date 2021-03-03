6f184970a9fe92203e437700a32c950006e54baf
figobaek <goldenboyy0524@gmail.com>
2021-03-03 18:50:26 +0900

    g

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,4 @@
 
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@127.0.0.1:3306/dreamteam_db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@127.0.0.1/dreamteam_db?reconnect=true'
+#CLEARDB_BLUE_URL = 'mysql+pymysql://b89a3294f37641:fa45866a@us-cdbr-east-03.cleardb.com/heroku_d1a097b04aacc37?reconnect=true'
