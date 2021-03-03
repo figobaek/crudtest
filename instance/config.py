546df5d84fc4685ea4c4fb80e11284665454de1d
figobaek <goldenboyy0524@gmail.com>
2021-03-03 13:01:57 +0900

    gogo

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,3 @@
 
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://:dt2021@127.0.0.1/dreamteam_db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://:dt2021@127.0.0.1:3306/dreamteam_db'
